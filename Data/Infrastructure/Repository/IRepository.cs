9f5c43f7cd5da663be341a5237aaea4e554cd47a
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2020-07-05 23:11:23 +0300

    generic repository interface created

---
+++ Data/Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Data.Infrastructure.Repository
+{
+    public interface IRepository<T> where T:class , new()
+    {
+        T Get(Expression<Func<T,bool>> filter = null); //select by filter
+        List<T> GetList(Expression<Func<T, bool>> filter = null); //return list if filter is applied
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
