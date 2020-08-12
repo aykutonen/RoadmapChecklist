3565ae0ff19e6314430c0c0f14083e8cfe5aa883
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2020-08-12 21:49:51 +0300

    user service created

---
+++ Service/User/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.User
+{
+    public interface IUserService : ISave
+    {
+        void Create(Entity.Domain.User.User userEntity);
+        void Update(Entity.Domain.User.User userEntity);
+        IEnumerable<Entity.Domain.User.User> Get();
+        void Delete(int userId);
+    }
+}
---
+++ Service/User/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Data.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Infrastructure.UnitOfWork;
+
+namespace Service.User
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<Entity.Domain.User.User> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserService(IRepository<Entity.Domain.User.User> repository, IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Entity.Domain.User.User userEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Entity.Domain.User.User userEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Entity.Domain.User.User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
