7fa573d7c9563b289532299746cde4e35dab8b93
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2020-09-03 14:30:36 +0300

    minor  changes

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -52,24 +52,6 @@ namespace Api.Controllers
             return createdUser.IsSuccess ? StatusCode(201) : BadRequest();
         }
 
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
-        }
-
         [HttpPost("login")]
         public IActionResult Login ([FromBody]UserLoginModel userLoginModel)
         {
