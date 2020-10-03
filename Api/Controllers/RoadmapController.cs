5fc5a3d5e9f566ece8715c790e8dbd3d464d5ee5
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2020-10-04 00:00:44 +0300

    delete roadmap endpoint created

--- Api/Controllers/RoadmapController.cs
+++ Api/Controllers/RoadmapController.cs
@@ -7,6 +7,7 @@ using Entity.Domain.Roadmap;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Service;
 using Service.Roadmap;
 
 namespace Api.Controllers
@@ -74,6 +75,26 @@ namespace Api.Controllers
                 return NotFound();
             }
             
+        }
+
+        [HttpDelete("deleteRoadmap")]
+        public IActionResult Delete([FromQuery] int roadmapId)
+        {
+            if (!ModelState.IsValid || roadmapId <= 0)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var roadmapResult = _roadmapService.Get(roadmapId);
+            var roadmapEntity = roadmapResult.IsSuccess ? roadmapResult.Data : null;
+
+            if (roadmapEntity != null)
+            {
+                var deletedRoadmap = _roadmapService.Delete(roadmapEntity);
+                return deletedRoadmap.IsSuccess ? StatusCode(204) : NotFound();
+            }
+            
+            return BadRequest();
         }
     }
 }
