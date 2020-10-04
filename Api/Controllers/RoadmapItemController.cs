d85a9afb2edaa12226138cae8653f6adc95f4def
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2020-10-04 22:56:31 +0300

    add roadmap item endpoint created

---
+++ Api/Controllers/RoadmapItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models.Roadmap;
+using Entity.Domain.Roadmap;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Service.Roadmap.RoadmapItem;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoadmapItemController : Controller
+    {
+        private IRoadmapItemService _roadmapItemService;
+        private IConfiguration _configuration;
+
+        public RoadmapItemController(IRoadmapItemService roadmapItemService, IConfiguration configuration)
+        {
+            _roadmapItemService = roadmapItemService;
+            _configuration = configuration;
+        }
+
+        [HttpPost("addToRoadmap")]
+        public IActionResult Add([FromBody] RoadmapItemModel roadmapItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var roadmapItemToAdd = new RoadmapItem()
+            {
+                Title = roadmapItem.Title,
+                Description = roadmapItem.Description,
+                Status = roadmapItem.Status,
+                TargetDate = roadmapItem.TargetDate,
+                EndDate = roadmapItem.EndDate,
+                RoadmapId = roadmapItem.RoadmapId,
+                ParentId = roadmapItem.ParentId,
+            };
+            var createdRoadmapItem = _roadmapItemService.Create(roadmapItemToAdd);
+
+            return createdRoadmapItem.IsSuccess ? StatusCode(201) : BadRequest();
+        }
+    }
+}
---
+++ Api/Models/Roadmap/RoadmapItemModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models.Roadmap
+{
+    public class RoadmapItemModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+        public DateTime TargetDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int RoadmapId { get; set; }
+        public int ParentId { get; set; }
+    }
+}
