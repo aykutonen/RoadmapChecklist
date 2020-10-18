8b9b9f4b6cff36e88130766159b843635643e2a6
hgunesdogdu <hande.gunesdogdu@hepsiburada.com>
2020-10-18 23:29:53 +0300

    get all roadmap information method edited

---
+++ Api/Models/ResponseModels/RoadmapWithItemsResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Entity.Domain.Roadmap;
+
+namespace Api.Models.ResponseModels
+{
+    public class RoadmapWithItemsResponseModel
+    {
+        public Entity.Domain.Roadmap.Roadmap Roadmap { get; set; }
+        public IEnumerable<RoadmapItem> RoadmapItems { get; set; }
+    }
+}
