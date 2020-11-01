00ddd6e033907dcf7a7787266ad368de1a3c575b
Aykut Önen <aykutonen@gmail.com>
2020-11-01 23:47:51 +0300

    BaseEntity modeli oluşturuldu.

---
+++ Entity/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Entity
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            var dt = DateTime.UtcNow;
+            this.CreateAt = dt;
+            this.UpdateAt = dt;
+            this.Status = 1;
+        }
+
+        public int Id { get; set; }
+        public int Status { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
