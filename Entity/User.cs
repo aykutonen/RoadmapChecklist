b7fe276001174462e23eb21afe3d0a573c0175b6
Aykut Önen <aykutonen@gmail.com>
2020-11-02 00:18:17 +0300

    User modeli tanımlandı.

---
+++ Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Picture { get; set; }
+
+        // Relations
+        public virtual Roadmap Roadmap { get; set; }
+
+    }
+}
