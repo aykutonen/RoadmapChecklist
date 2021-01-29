c5d8946bbc100ef40d1f513063cfd315a6d12629
Aykut Önen <aykutonen@gmail.com>
2021-01-29 06:16:44 +0300

    aktif kullanıcı id bilgisi ve kontrolü oluşturulan BaseController'a taşındı.

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,24 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Web.Models;
 
 namespace Web.Controllers
 {
     [Authorize]
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController(ILogger<HomeController> logger) : base(logger) { }
 
         public IActionResult Index()
         {
